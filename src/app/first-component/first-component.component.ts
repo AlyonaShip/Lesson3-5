d8c270a619e076aeebe3282592362a7d5db9610a
AlyonaShip <AlyonaShip>
2021-07-29 02:00:34 +0300

    complete

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-first-component',
@@ -8,22 +9,83 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FirstComponentComponent implements OnInit {
 
-  public userName!: string;
-  constructor(private httpClient: HttpClient) { }
+  promotionApplicationForm!: FormGroup;
+  promotionApplication!: PromotionApplication;
+
+  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder) {
+    this.buildForm();
+    this.populateForm();
+  }
 
   ngOnInit(): void {
   }
 
-  public nameConfirmed() {
-    if (this.userName) {
-      localStorage.setItem('userName', this.userName);
-    }
-  }
-
-  public sendRequest() {
-    this.httpClient.get<any>('https://jsonplaceholder.typicode.com/posts').subscribe(result => {
-      console.table(result);
+  private buildForm() {
+    this.promotionApplicationForm = this.formBuilder.group({
+      firstName: ['', { validators: this.validateName, updateOn: 'blur'}],
+      lastName: ['', Validators.required],
+      address: new FormGroup({
+        city: new FormControl('', [Validators.required]),
+        district: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('', [Validators.required])
+      }),
+      position: ['', Validators.required],
+      salary: ['', Validators.required],
+      reason: ['', Validators.required]
     });
   }
 
+  private populateForm() {
+    this.promotionApplication = JSON.parse(localStorage.getItem('promotionApplication')!);
+
+    if (this.promotionApplication) {
+      this.promotionApplicationForm.setValue(this.promotionApplication);
+    }
+  }
+
+  public clearData() {
+    localStorage.clear();
+    this.promotionApplicationForm.reset();
+  }
+
+  public onSubmit() {
+    this.promotionApplication = this.promotionApplicationForm.value;
+    localStorage.setItem('promotionApplication', JSON.stringify(this.promotionApplication));
+  }
+
+  public isControlInvalid(controlName: string) {
+    let control = this.promotionApplicationForm.get(controlName);
+    return !control?.valid;
+  }
+
+  public isErrorExist(controlName: string, errorType: string) {
+    let control = this.promotionApplicationForm.get(controlName);
+
+    if (control) {
+      return control.errors![errorType];
+    }
+    return false;
+  }
+
+  public validateName(control: AbstractControl): ValidationErrors {
+    if (control.value === 'COM9') {
+      return { invalidValue: true };
+    }
+    return null!;
+  }
+}
+
+export class PromotionApplication {
+  firstName!: string;
+  lastName!: string;
+  address!: OfficeAddress;
+  position!: string;
+  salary!: number;
+  reason!: string;
+}
+
+export class OfficeAddress {
+  city!: string;
+  district!: string;
+  zipCode!: string;
 }
